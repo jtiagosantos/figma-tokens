5e699ffe76b418ac3ec94b1253d600ca438ec99d
jtiagosantos <tiagosan040@gmail.com>
2022-12-02 18:40:51 -0300

    initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,14 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 02 Dec 2022 21:37:21 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#808080",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple50": "#cbbeeb",
+  "ColorPurple500": "#8257e6",
+  "ColorRed500": "#e53e3e"
+};
